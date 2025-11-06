390c74beb2acb782f99c45ab55f2a8762ad46302
mohamadahmadisadr <mohamadahmadisadr@gmail.com>
2025-11-05 21:36:09 -0500

    define user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,1 +1,11 @@
 package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID	primitive.ObjectID `bson:"_id.omitempty"`
+	Name string `bson:"name"`
+	Email string `bson:"email"`
+	UserType string `bson:"userType"`
+
+}
