3efbc4695c16298265841b6f1c9f4b7a7bdfbf85
mohamadahmadisadr <mohamadahmadisadr@gmail.com>
2025-11-05 21:49:14 -0500

    mongo db connection extablished

--- config/config.go
+++ config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		log.Println("MONGO URI not set, using default uri")
+		mongoURI = "mongodb://localhost:27017"
+	}
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	//Set Context with timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel() //Avoid context leak
+
+	//connect to mongoDB
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal("mongo connection failed : %v ", err)
+	}
+
+	log.Println("Connected Successfully")
+	return client
+
+}
